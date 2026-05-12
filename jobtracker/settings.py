6a9fca04e5249b9df8d932b3316cbc72a5dec3ca
wraith19 <svammsi0901@gmail.com>
2026-05-12 20:21:40 +0530

    feat: move SECRET_KEY to .env using python-decouple, added requirements.txt

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#yss(+ao$)j-+5i5uch1lb3)t5p@n$^q4h^yq=ri(j%9)$qd=l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
